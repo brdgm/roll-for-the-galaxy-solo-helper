231e54a1655cb72034c077e9b353723be184e50b
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-10-25 16:15:50 +0200

    remove unused component

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -33,13 +33,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
-import ModalDialog from 'brdgm-commons/src/components/structure/ModalDialog.vue'
 
 export default defineComponent({
   name: 'AppHome',
-  components: {
-    ModalDialog
-  },
   setup() {
     const { t } = useI18n()
     return { t }

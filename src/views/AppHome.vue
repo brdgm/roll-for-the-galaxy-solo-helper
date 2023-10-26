498add484f584ce64385b8d152ea7167244b098e
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-10-26 21:56:43 +0200

    add bgg and github link

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -11,13 +11,11 @@
   <p v-html="t('home.play1')"></p>
   <p v-html="t('home.play2')"></p>
 
-<!--
   <p class="text-muted fst-italic">
     {{t('home.feedback')}}
-    <a href="https://boardgamegeek.com/thread/XXX" target="_blank" rel="noopener">Board Game Geek Thread</a> |
+    <a href="https://boardgamegeek.com/thread/3178925/brdgmme-roll-galaxy-solo-helper-web-application" target="_blank" rel="noopener">Board Game Geek Thread</a> |
     <a href="https://github.com/brdgm/roll-for-the-galaxy-solo-helper" target="_blank" rel="noopener">GitHub</a>
   </p>
--->
 
   <router-link to="/setupGame" class="btn btn-primary btn-lg mt-3">{{t('action.playGame')}}</router-link>
 
